38ae38b4643312e9086d0c6db8dc4444bc78adcb
dfoxlee <david.foxlee@gmail.com>
2026-02-17 16:39:45 -0500

    updated files for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 
 import usersRouter from "./routes/users.routes.js";
 import workoutsRouter from "./routes/workouts.routes.js";
@@ -45,16 +45,16 @@ app.use("/api/v1/completed-workouts", authMiddleware, completedWorkoutsRouter);
 app.use("/api/v1/completed-exercises", authMiddleware, completedExerciseRouter);
 
 // Get __dirname equivalent in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // Serve static files from frontend dist folder
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Catch-all route: serve index.html for all non-API routes (SPA routing)
-app.get("/{*any}", (req, res) => {
-   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+// app.get("/{*any}", (req, res) => {
+//    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// });
 
 // error middleware
 app.use(errorMiddleware);
